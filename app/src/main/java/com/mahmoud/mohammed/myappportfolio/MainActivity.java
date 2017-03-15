111e9fe0ac4442dd42ea122e9a5a7f8319343afb
mohammed mahmoud <mohammed mahmoud>
2017-03-15 09:58:53 +0200

    feat:add methods for each button

--- app/src/main/java/com/mahmoud/mohammed/myappportfolio/MainActivity.java
+++ app/src/main/java/com/mahmoud/mohammed/myappportfolio/MainActivity.java
@@ -12,6 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void run_movie(View view) {
+        Toast.makeText(MainActivity.this,"run Movie App",Toast.LENGTH_SHORT).show();
+    }
+    public void run_stock(View view) {
+        Toast.makeText(MainActivity.this,"run stocks App",Toast.LENGTH_SHORT).show();
+    }
+    public void run_build(View view) {
+        Toast.makeText(MainActivity.this,"run Build It Bigger App",Toast.LENGTH_SHORT).show();
+    }
+    public void run_xyz(View view) {
+        Toast.makeText(MainActivity.this,"run Xyz Readr App",Toast.LENGTH_SHORT).show();
+    }
+    public void run_wear(View view) {
+        Toast.makeText(MainActivity.this,"run Go Ubiquitous App",Toast.LENGTH_SHORT).show();
+    }
+    public void run_capa(View view) {
+        Toast.makeText(MainActivity.this,"run Capastone App",Toast.LENGTH_SHORT).show();
+    }
+
 
 
 
